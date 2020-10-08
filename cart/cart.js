6d185d81c26b61222878bf4a88321f5151b22435
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-10-08 14:02:29 -0700

    local storage cart

--- cart/cart.js
+++ cart/cart.js
@@ -1,20 +1,21 @@
-import { beers, cart } from '../data.js';
+import { beers } from '../data.js';
 import { beerCartRender } from '../cart/cartRenderUtil.js';
 import { findById } from '../utils.js';
 import { calcTotal } from '../cart/cart-utils.js';
+import { getFromLocalStorage, CART } from '../renderUtils.js';
+
 
 // import { getFromLocalStorage, CART } from '../renderUtils.js'
-
+const orderButton = document.getElementById('order-button');
 const table = document.querySelector('tbody');
-// const cart = getFromLocalStorage(CART) || [];
+const cart = getFromLocalStorage(CART) || [];
 
 for (let i = 0; i < cart.length; i++) {
     const beer = cart[i];
 
-    if (beer.quantity >= 0) {
-        const tr = beerCartRender(beer);
-        table.appendChild(tr);
-    }
+
+    const tr = beerCartRender(beer);
+    table.appendChild(tr);
 }
 
 const total = calcTotal(cart, beers);
@@ -24,14 +25,13 @@ const totalCell = document.querySelector('.total');
 totalCell.textContent = `Total: $${total}`;
 
 
+orderButton.addEventListener('click', () => {
+    const stringyCart = JSON.stringify(cart, true, 2);
+    alert(stringyCart);
 
-// orderButton.addEventListener('click', () => {
-//     const stringyCart = JSON.stringify(cart, true, 2);
-//     alert(stringyCart);
-
-//     localStorage.clear();
-//     window.location.href = '/';
-// });
+    localStorage.clear();
+    window.location.href = '../index.html';
+});
 
 
 
